67f4e7b0d45592b418bb057ce1bb08cd2cd917bd
Igor Makarov <igormaka@gmail.com>
2021-07-16 16:46:10 +0300

    add spec for native target

--- spec/censorius_spec.rb
+++ spec/censorius_spec.rb
@@ -72,4 +72,30 @@ RSpec.describe Censorius::UUIDGenerator do
       PBXProject(#{@spec_safe_name})/XCConfigurationList/XCBuildConfiguration(Release)
     ].map { |k| Digest::MD5.hexdigest(k).upcase }.sort
   end
+
+  it 'generates UUIDs for native targets' do
+    @project.new_target(:application, 'AppTarget', :ios)
+    @generator.generate!
+
+    expect(@project.objects_by_uuid.keys.sort).to eq %W[
+      PBXNativeTarget(AppTarget)/XCConfigurationList
+      PBXNativeTarget(AppTarget)/XCConfigurationList/XCBuildConfiguration(Debug)
+      PBXNativeTarget(AppTarget)/XCConfigurationList/XCBuildConfiguration(Release)
+      PBXProject(#{@spec_safe_name})
+      PBXProject(#{@spec_safe_name})/PBXFileReference(${BUILT_PRODUCTS_DIR}/AppTarget.app)
+      PBXProject(#{@spec_safe_name})/PBXFileReference(${DEVELOPER_DIR}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.0.sdk/System/Library/Frameworks/Foundation.framework)
+      PBXProject(#{@spec_safe_name})/PBXGroup(/)
+      PBXProject(#{@spec_safe_name})/PBXGroup(/Frameworks)
+      PBXProject(#{@spec_safe_name})/PBXGroup(/Frameworks/iOS)
+      PBXProject(#{@spec_safe_name})/PBXGroup(/Products)
+      PBXProject(#{@spec_safe_name})/PBXNativeTarget(AppTarget)
+      PBXProject(#{@spec_safe_name})/PBXNativeTarget(AppTarget)/PBXFrameworksBuildPhase(Frameworks)
+      PBXProject(#{@spec_safe_name})/PBXNativeTarget(AppTarget)/PBXFrameworksBuildPhase(Frameworks)/PBXBuildFile(PBXProject(#{@spec_safe_name})/PBXFileReference(${DEVELOPER_DIR}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.0.sdk/System/Library/Frameworks/Foundation.framework))
+      PBXProject(#{@spec_safe_name})/PBXNativeTarget(AppTarget)/PBXResourcesBuildPhase(Resources)
+      PBXProject(#{@spec_safe_name})/PBXNativeTarget(AppTarget)/PBXSourcesBuildPhase(Sources)
+      PBXProject(#{@spec_safe_name})/XCConfigurationList
+      PBXProject(#{@spec_safe_name})/XCConfigurationList/XCBuildConfiguration(Debug)
+      PBXProject(#{@spec_safe_name})/XCConfigurationList/XCBuildConfiguration(Release)
+    ].map { |k| Digest::MD5.hexdigest(k).upcase }.sort
+  end
 end
